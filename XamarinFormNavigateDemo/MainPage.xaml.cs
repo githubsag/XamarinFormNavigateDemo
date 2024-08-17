52bb11d86504aefe9f91e6899866e19cfd3b7484
sohrab <azamighadim2@gmail.com>
2024-08-17 22:54:09 +0200

    added entire XamarinFormNavigateDemo Xamrin forms project

---
+++ XamarinFormNavigateDemo/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinFormNavigateDemo
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void NavigateButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+
+        void Button_onClicked(System.Object sender, System.EventArgs e)
+        {
+            string text = MainEntry.Text;
+            MainLabel.Text = "Hello " + text;
+        }
+    }
+}
